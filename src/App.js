733a6df97380eead5f9c217d80565a1c42ca38a7
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-05-18 00:40:49 +0530

    components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 
-function App() {
+function App(props) {
+  console.log(props.tasks);
   return (
     <>
       <BrowserRouter>
