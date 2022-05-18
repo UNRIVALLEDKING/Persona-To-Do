d02ea4e9e65f106c569a75c6b5f8d14588a01406
UNRIVALLEDKING <adityakumarunrivalledking@gmail.com>
2022-05-18 18:14:02 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
+import FilterButton from "./Components/FilterButton";
+import Forms from "./Components/Forms";
+import Todo from "./Components/Todo";
 
 function App(props) {
   console.log(props.tasks);
+  // const taskList = props.tasks?.map((aditya) => aditya.name);
+
+  const myTodo = props.tasks?.map((aditya) => (
+    <Todo
+      id={aditya.id}
+      name={aditya.name}
+      completed={aditya.completed}
+      key={aditya.id}
+    />
+  ));
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="*" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="todoapp stack-large">
+        <h1>Persona To Do</h1>
+        <Forms />
+        <div className="filters btn-group stack-exception">
+          <FilterButton />
+          <FilterButton />
+          <FilterButton />
+        </div>
+        <h2 id="list-heading">3 tasks remaining</h2>
+        <ul
+          role="list"
+          className="todo-list stack-large stack-exception"
+          aria-labelledby="list-heading"
+        >
+          {/* {taskList} */}
+
+          {myTodo}
+        </ul>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const DATA = [
   {
     id: "todo-0",
@@ -26,8 +27,6 @@ const DATA = [
     completed: false,
   },
 ];
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App tasks={DATA} />
